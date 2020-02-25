126c2cc009ad3334119118695fde48ffc666df8a
Petri Vuorimaa <petri.vuorimaa@aalto.fi>
2020-02-25 18:07:59 +0200

    apexTest component removed

--- force-app/main/default/lwc/apexTest/apexTest.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import getProcessList from '@salesforce/apex/AnnualClock.getProcessList';
-
-export default class ApexTest extends LightningElement {
-    @wire(getProcessList) processes;
-}
